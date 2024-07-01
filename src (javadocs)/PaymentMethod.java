46452c98ef7b91aa0e3dda23c6f7f6f659799956
Yew Joe <157005956+YewJoeLee@users.noreply.github.com>
2024-07-01 15:07:51 +0800

    Delete src (javadocs) directory

--- src (javadocs)/PaymentMethod.java
+++
@@ -1,9 +0,0 @@
-/**
- * The {@code PaymentMethod} interface defines a contract for payment methods.
- * Classes implementing this interface must provide an implementation for the
- * {@code finalPayment()} method.
- */
-
-public interface PaymentMethod {
-	public boolean finalPayment();
-}
--- src (javadocs)/Permutable.java
+++
@@ -1,8 +0,0 @@
-/**
- * The {@code Permutable} abstract class extends the {@code BranchApp} class
- * and provides an abstract method {@code request()} that must be implemented
- * by concrete subclasses.
- */
-public abstract class Permutable extends BranchApp{
-    public abstract void request();
-}
